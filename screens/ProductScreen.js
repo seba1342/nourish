908b6cc643f53249c89c59cc937f48bac4f57e66
seba1342 <sbailouni@gmail.com>
2019-09-30 21:30:54 +1000

    Created baseline product page

--- screens/ProductScreen.js
+++ screens/ProductScreen.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import {View, Text, Button} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+import {Colors} from 'react-native/Libraries/NewAppScreen';
 
 class ProductScreen extends React.Component {
   static navigationOptions = {
@@ -8,11 +18,107 @@ class ProductScreen extends React.Component {
   render() {
     const {navigate} = this.props.navigation;
     return (
-      <View>
-        <Text>Products</Text>
-      </View>
+      <>
+        <StatusBar barStyle="dark-content" />
+        <SafeAreaView>
+          <ScrollView>
+            <View>
+              <Image
+                style={styles.productImage}
+                source={require('../assets/images/image.jpg')}
+              />
+            </View>
+            <View style={styles.body}>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>Code Nourish.</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step Two</Text>
+                <Text style={styles.sectionDescription}>Profit</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>Code Nourish.</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step Two</Text>
+                <Text style={styles.sectionDescription}>Profit</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>Code Nourish.</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step Two</Text>
+                <Text style={styles.sectionDescription}>Profit</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>Code Nourish.</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step Two</Text>
+                <Text style={styles.sectionDescription}>Profit</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step One</Text>
+                <Text style={styles.sectionDescription}>Code Nourish.</Text>
+              </View>
+              <View style={styles.sectionContainer}>
+                <Text style={styles.sectionTitle}>Step Two</Text>
+                <Text style={styles.sectionDescription}>Profit</Text>
+              </View>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: 'transparent',
+    marginTop: 200,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  productImage: {
+    width: '100%',
+    height: 200,
+    top: 0,
+    left: 0,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+    bottom: 0,
+  },
+});
 
 export default ProductScreen;
