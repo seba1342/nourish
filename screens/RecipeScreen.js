d313dcc615adc39f6009478162a090f1f00e6352
Jonathon Vrankul <jvra0001@student.monash.edu>
2019-10-07 16:35:32 +1100

    Add unity package

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
+import UnityView from 'react-native-unity-view';
 
 class RecipeScreen extends React.Component {
   static navigationOptions = {
@@ -7,8 +8,22 @@ class RecipeScreen extends React.Component {
   };
   render() {
     const {navigate} = this.props.navigation;
+    let unityElement: JSX.Element;
+
+    unityElement = (
+      <UnityView
+        style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}
+      />
+    );
+
+    console.log(unityElement);
+
     return (
       <View style={styles.container}>
+        {/* <UnityView
+          style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}
+        /> */}
+        {unityElement}
         <Text>Recipes</Text>
       </View>
     );
