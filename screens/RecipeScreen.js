19610f1e8bd22892e16f4e8851759a1e04f2d40c
Jonathon Vrankul <jvra0001@student.monash.edu>
2019-10-07 17:09:12 +1100

    Remove unity stuff

--- screens/RecipeScreen.js
+++ screens/RecipeScreen.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import {StyleSheet, View, Text} from 'react-native';
-import UnityView from 'react-native-unity-view';
 
 class RecipeScreen extends React.Component {
   static navigationOptions = {
@@ -8,22 +7,9 @@ class RecipeScreen extends React.Component {
   };
   render() {
     const {navigate} = this.props.navigation;
-    let unityElement: JSX.Element;
-
-    unityElement = (
-      <UnityView
-        style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}
-      />
-    );
-
-    console.log(unityElement);
 
     return (
       <View style={styles.container}>
-        {/* <UnityView
-          style={{position: 'absolute', left: 0, right: 0, top: 0, bottom: 0}}
-        /> */}
-        {unityElement}
         <Text>Recipes</Text>
       </View>
     );
