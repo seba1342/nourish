9ed09884c04b3646a815c77c444f2374744b4893
Jonathon Vrankul <jvra0001@student.monash.edu>
2019-10-14 20:46:03 +1100

    Merge modal with product card chages

--- components/Product/ProductCards.js
+++ components/Product/ProductCards.js
@@ -8,6 +8,7 @@ import {
   TouchableHighlight,
 } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Button from 'apsl-react-native-button';
 
 import {Colors} from '../../assets/constants.js';
 
@@ -15,46 +16,59 @@ var ratingColor = '#123456';
 
 const ProductCards = product => {
   const [visible, setVisible] = useState(false);
+  const [score, setScore] = useState({});
 
   return (
     <>
       <Modal
         animationType="slide"
-        transparent={false}
+        transparent={true}
         visible={visible}
         onPress={() => {
           setVisible(!visible);
         }}>
-        <View>
-          <TouchableHighlight
-            onPress={() => {
-              setVisible(!visible);
-            }}>
-            <Text>Hide Modal</Text>
-          </TouchableHighlight>
+        <View style={styles.productModal}>
+          <View style={styles.productModalInner}>
+            <Text style={styles.modalTitle}>{score.title}</Text>
+            <Text style={styles.modalCopy}>{score.ratingDescription}</Text>
+            <TouchableHighlight style={styles.closeModal}>
+              <View>
+                <Button
+                  onPress={() => {
+                    setVisible(!visible);
+                  }}
+                  style={[styles.closeModal, styles.buttonSecondary]}
+                  textStyle={{color: Colors.light, fontSize: 18}}>
+                  Close
+                </Button>
+              </View>
+            </TouchableHighlight>
+          </View>
         </View>
       </Modal>
 
-      <TouchableHighlight
-        onPress={() => {
-          setVisible(!visible);
-        }}>
-        <View style={styles.productCardContainer}>
-          {product.product.score.map((item, key) => {
-            switch (item.rating) {
-              case 1:
-                ratingColor = Colors.green;
-                break;
-              case 2:
-                ratingColor = Colors.yellow;
-                break;
-              default:
-              case 3:
-                ratingColor = Colors.red;
-                break;
-            }
-            return (
-              <View style={styles.productCard}>
+      <View style={styles.productCardContainer}>
+        {product.product.score.map((item, key) => {
+          switch (item.rating) {
+            case 1:
+              ratingColor = Colors.green;
+              break;
+            case 2:
+              ratingColor = Colors.yellow;
+              break;
+            default:
+            case 3:
+              ratingColor = Colors.red;
+              break;
+          }
+          return (
+            <TouchableHighlight
+              style={styles.productCard}
+              onPress={() => {
+                setVisible(!visible);
+                setScore(item);
+              }}>
+              <View style={styles.productCardInner}>
                 <Icon
                   style={[{color: ratingColor}]}
                   size={64}
@@ -64,10 +78,10 @@ const ProductCards = product => {
                   {item.title}
                 </Text>
               </View>
-            );
-          })}
-        </View>
-      </TouchableHighlight>
+            </TouchableHighlight>
+          );
+        })}
+      </View>
     </>
   );
 };
@@ -75,6 +89,44 @@ const ProductCards = product => {
 export default ProductCards;
 
 const styles = StyleSheet.create({
+  closeModal: {
+    backgroundColor: Colors.primary,
+    color: Colors.light,
+    borderRadius: 30,
+    borderWidth: 0,
+    width: '50%',
+    alignSelf: 'center',
+    alignContent: 'center',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: Colors.primary,
+  },
+  modalCopy: {
+    fontSize: 16,
+    color: Colors.primary,
+    paddingHorizontal: 20,
+    textAlign: 'center',
+  },
+  productModal: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#00000080',
+  },
+  productModalInner: {
+    width: '80%',
+    height: '80%',
+    backgroundColor: '#FFFFFF',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   productCardContainer: {
     flexDirection: 'row',
     flexWrap: 'wrap',
@@ -94,6 +146,15 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.1,
     shadowRadius: 10,
     elevation: 3,
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+    textAlign: 'center',
+  },
+  productCardInner: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: Colors.light,
+    borderRadius: 10,
     justifyContent: 'space-evenly',
     alignItems: 'center',
     textAlign: 'center',
